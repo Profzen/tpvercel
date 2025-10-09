f951fcb8f7a974fcf5786e10d0880e04ae413079
LIMDEYOU Médar Essossinam <mavensonar@gmail.com>
2025-10-09 17:07:55 +0000

    My first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Contact from "./components/contact"
+import Navbar from "./components/Navbar"
+import Newsletter from "./components/Newsletter"
+import Prices from "./components/Prices"
+import Team from "./components/team"
+import Testimonials from "./components/Testimonials"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Team />
+      <Prices />
+      <Testimonials />
+      <Newsletter />
+      <Contact />
+    </>
+  )
+}
+
+export default App
